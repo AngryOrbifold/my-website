312c9a364ab29c2601468208d43d62584f741c9e
AngryOrbifold <angryorbifold@gmail.com>
2026-01-24 19:14:49 +0100

    Update quiz.js

--- NOFRAT/quiz.js
+++ NOFRAT/quiz.js
@@ -649,6 +649,7 @@ window.addEventListener("focus", () => setTimeout(resyncFromServer, 200));
 document.addEventListener("visibilitychange", () => {
   if (document.visibilityState === "visible") setTimeout(resyncFromServer, 200);
 });
-setInterval(resyncFromServer, 90_000);
+setInterval(resyncFromServer, 300_000);
 loadUserProgress();
+
 
