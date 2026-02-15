279e5151692f0bee4de0c1d772693df84025938a
AngryOrbifold <angryorbifold@gmail.com>
2026-02-15 16:36:38 +0100

    Update quiz.js

--- NOACT/quiz.js
+++ NOACT/quiz.js
@@ -288,13 +288,9 @@ function showFinalResults() {
       <button id="changeUsernameBtn">Change Username</button>
     </div>
 
-    <div class="section"; margin-top:20px;">
-      <p>You can see your certificate by inputting your email adress in the field below:</p>
-    </div>
-
     <form id="certForm" style="margin-top: 20px;">
       <label>Email:</label>
-      <input type="email" id="email" required />
+      <input type="email" id="email" value="${email}" readonly />
       <button type="submit" class="button">Show Certificate</button>
     </form>
 
@@ -449,4 +445,5 @@ darkModeBtn?.addEventListener("click", () => {
 });
 
 loadUserProgress();
+
 
